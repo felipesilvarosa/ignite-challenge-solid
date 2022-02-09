954ed19d24102d33d06a113a325ecfa9f0162142
Felipe Rosa <felipesrosa@gmail.com>
2022-02-09 15:15:43 -0300

    Criado rota para cadastro de usuario

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,15 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  name?: string;
+  email?: string;
+  id?: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
 
 export { User };
